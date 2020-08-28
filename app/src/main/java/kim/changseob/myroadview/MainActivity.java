dd9d2da894ade7681b8fe30b68382544c6bedeeb
changseob <seobing92@gmail.com>
2020-08-28 22:00:51 +0900

    Draw Kakao Map using API

--- app/src/main/java/kim/changseob/myroadview/MainActivity.java
+++ app/src/main/java/kim/changseob/myroadview/MainActivity.java
@@ -1,30 +1,149 @@
 package kim.changseob.myroadview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.util.Log;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+import net.daum.mf.map.api.MapLayout;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements MapView.OpenAPIKeyAuthenticationResultListener, MapView.MapViewEventListener{
+
+    final String TAG = "MainActivity";
+
+    String[] permissions = new String[]{
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA
+    };
+
+
+    private MapView mMapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        checkPermissions();
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
-        TextView tv = findViewById(R.id.sample_text);
-        tv.setText(stringFromJNI());
+        ViewGroup mapViewContainer = (ViewGroup) findViewById(R.id.map_view_root);
+        MapLayout mapLayout = new MapLayout(this);
+        mMapView = mapLayout.getMapView();
+        mMapView.setOpenAPIKeyAuthenticationResultListener(this);
+        mMapView.setMapViewEventListener(this);
+        mMapView.setDaumMapApiKey(getKaKaoAPIKeyFromManifest());
+        mMapView.setMapType(MapView.MapType.Standard);
+        mapViewContainer.addView(mapLayout);
+//        /Log.e(TAG, "dsf");
+
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+
+    private String getKaKaoAPIKeyFromManifest() {
+        String key = "";
+        try {
+            ApplicationInfo app = getPackageManager().getApplicationInfo(getPackageName(),PackageManager.GET_META_DATA);
+            Bundle bundle = app.metaData;
+            key = bundle.getString("com.kakao.sdk.AppKey");
+
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        }
+        return key;
+    }
+
+    private boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), 100);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        if (requestCode == 100) {
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // do something
+            }
+            return;
+        }
+    }
+
+    @Override
+    public void onDaumMapOpenAPIKeyAuthenticationResult(MapView mapView, int i, String s) {
+        Log.i(TAG,	String.format("Open API Key Authentication Result : code=%d, message=%s", i, s));
+    }
+
+    @Override
+    public void onMapViewInitialized(MapView mapView) {
+        Log.i(TAG, "MapView had loaded. Now, MapView APIs could be called safely");
+        mapView.setMapCenterPointAndZoomLevel(MapPoint.mapPointWithGeoCoord(37.537229,127.005515), 2, true);
+    }
+
+    @Override
+    public void onMapViewCenterPointMoved(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewZoomLevelChanged(MapView mapView, int i) {
+
+    }
+
+    @Override
+    public void onMapViewSingleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDoubleTapped(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewLongPressed(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragStarted(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewDragEnded(MapView mapView, MapPoint mapPoint) {
+
+    }
+
+    @Override
+    public void onMapViewMoveFinished(MapView mapView, MapPoint mapPoint) {
+
+    }
 }
